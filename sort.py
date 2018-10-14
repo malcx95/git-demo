d4609f403b451adf2c23d873ea5065d8b0f60d58
Kurt <trekommafem2@gmail.com>
2018-10-14 19:59:47 +0200

    Add docstring for my shitty code

--- sort.py
+++ sort.py
@@ -1,5 +1,8 @@
 
 def sort(seq):
+    """
+    Returns a sorted version of seq
+    """
     a = False
     while not a:
         a = True
@@ -9,4 +12,5 @@ def sort(seq):
                 seq[i + 1] = seq[i]
                 seq[i] = temp
                 a = False
+    return seq
 
